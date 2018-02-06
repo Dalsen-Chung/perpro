d54f90b0771ba870c03ed55878605cb71a1f00b1
Dalsen <Codedalsen@163.com>
2018-02-06 18:03:33 +0800

    commit files

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
-// window.onload = function() {
-//   window.addEventListener('hashchange', function() {
-//     console.log('hashchange');
-//     console.log(window.location);
-//   });
-// }
+function InitBar(TargetClass,father_C,bar_C){
+  this.TargetClass = TargetClass;
+  this.fatherClass = father_C;
+  this.bar_C = bar_C;
+  this.barLen = 0;
+  this.bindEvent = function(){
+    document.addEventListener('mousewheel',this.mousewheel);
+  };
+  this.mousewheel = function(e){
+    let event = e || window.event;
+    console.log(e);
+  }
+  this.initBarLen = function(){
+    let father_len = document.getElementsByClassName(this.TargetClass)[0].clientHeight;
+    let fa_scroll_len = document.getElementsByClassName(this.fatherClass)[0].clientHeight;
+    let scrollBar = document.getElementsByClassName(this.bar_C)[0];
+    console.log(scrollBar.clientHeight);
+    scrollBar.style.height = (fa_scroll_len - father_len + fa_scroll_len) + 'px';
+  }
+}
+
+let bar = new InitBar('menu','aside-scroll','scrollbar');
+bar.initBarLen();
+bar.bindEvent();
