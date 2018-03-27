6dbd24af24c88e858399fefc8ad3592c44e1ad7c
Dalsen <Codedalsen@163.com>
2018-03-27 11:15:59 +0800

    commit files

--- jsDesignMode/DecoratorMode.js
+++ jsDesignMode/DecoratorMode.js
@@ -2,14 +2,23 @@
 //新的方法时先调用原有的方法保证原有方法不受影响。
 
 let decorator = function (dom, type, callback) {   //对监听事件的装饰 以DOM2级为案例
-  let element = typeof dom === 'string' ? document.querySelector('#'+ dom) : dom;
-  if ( typeof dom[type] === 'function' ){
-    let oldFn = dom[type];
-    dom[type] = function () {
+  let element = typeof dom === 'string' ? document.querySelector(dom) : dom;
+  if ( typeof element[type] === 'function' ){
+    let oldFn = element[type];
+    element[type] = function () {
       oldFn();
       callback();
     }
   }else{
-    dom[type]  = callback;
+    element[type]  = callback;
   }
 }
+
+let btn = document.querySelector('#btn');    // eg : 为btn的点击事件添加新的功能
+btn.onclick = function() {
+  console.log('old!');
+}
+
+decorator('#btn','onclick',function(){
+  console.log('hahahah');
+});
