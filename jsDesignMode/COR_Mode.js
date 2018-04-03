6bcaa17558e86617e4427ab6e5fb48dec15281d3
Dalsen <Codedalsen@163.com>
2018-04-03 11:03:48 +0800

    commit files

--- jsDesignMode/COR_Mode.js
+++ jsDesignMode/COR_Mode.js
@@ -1,1 +1,27 @@
 //chain of responsibility  职责链模式
+let getdata = function () {
+  $.ajax({
+    type : 'GET',
+    url : '',
+    success : function(data) {
+      dealData(data);                   //职责一
+    }
+  })
+};
+
+let dealData = function (data) {
+  let arr = data.split(',');
+  saveData(arr);                      //职责二
+};
+
+let saveData = function (arr) {
+  $.ajax({
+    type : 'post',
+    url : '',
+    success : function (data) {
+
+    }
+  })
+};
+
+getdata();   //调用总职责
